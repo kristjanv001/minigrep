7e67148c7ef5f6137b7515b726e0c8906aa766b3
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2024-12-18 22:43:05 +0200

    eprintln instead of println for errors

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use std::process;
 fn main() {
   let args: Vec<String> = env::args().collect();
   let config = Config::build(&args).unwrap_or_else(|err| {
-    println!("Problem with arguments: {err}");
+    eprintln!("Problem with parsing arguments: {err}");
     process::exit(1);
   });
 
   if let Err(e) = minigrep::run(config) {
-    println!("App error: {e}");
+    eprintln!("App error: {e}");
     process::exit(1);
   }
 }
